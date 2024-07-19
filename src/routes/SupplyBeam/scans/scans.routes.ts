a3aa409af79c7cf20a6b4c0aae1a52dc2fbbb1fb
tarun <tarun.bhardwaj@genuinemark.org>
2024-07-19 15:41:26 +0530

    verify qr code at checin route

--- src/routes/SupplyBeam/scans/scans.routes.ts
+++ src/routes/SupplyBeam/scans/scans.routes.ts
@@ -1,5 +1,6 @@
 import { protectSupplyBeam } from "../../../../middleware/auth.middleware";
 import { basicQrValidation } from "../../../../middleware/scan.middleware";
+import { verifyQrCodeAtCheckin } from "../../../controller/scans/checkin.scan.controller";
 import { verifyQrCodeAtCheckout } from "../../../controller/scans/checkouts.scan.controller";
 
 
@@ -8,6 +9,7 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/checkout-in").get(protectSupplyBeam, basicQrValidation , verifyQrCodeAtCheckout );
+router.route("/checkin-in").get(protectSupplyBeam, basicQrValidation , verifyQrCodeAtCheckin );
 
 
 export default router;
